803ef2d7772155afb92b172a2e3b57d66ba55a6a
donahue.joe.t <donahue.joe.t@gmail.com>
2022-01-10 17:13:19 -0600

    Added a node method to calculate the root.

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,3 +1,9 @@
 class Node < ApplicationRecord
-
+  # This method extracts the root from the node path.
+  def root
+    # Use the node_id if the path contains a '.' otherwise use the number in
+    # front of the first '.' Alternatively this could check to see if parent_id
+    # is blank instead of seeing if the path contains a '.'
+    path.index('.') ? path[0, path.index('.')] : node_id
+  end
 end
