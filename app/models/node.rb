c2b93a6443dcc9b9a27d1b0ef2ab4eaec5d942a0
donahue.joe.t <donahue.joe.t@gmail.com>
2022-01-11 00:18:00 -0600

    Added more commenting to the node model.

--- app/models/node.rb
+++ app/models/node.rb
@@ -63,11 +63,15 @@ class Node < ApplicationRecord
     path.split('.')
   end
 
+  # This method sets the node paths for the passed nodes.
   def self.set_node_paths(nodes)
+    # Initialize an array to hold the nodes that did not have paths added.
     not_added = []
 
+    # Get the node count before the loop.
     count = nodes.count
 
+    # Loops through the nodes.
     nodes.each do |node|
       # Skip this record. If the parent id is blank then it is a root node and
       # the path has already been added.
@@ -75,21 +79,31 @@ class Node < ApplicationRecord
         next
       end
       
+      # Get the node parent.
       parent = Node.where(node_id: node.parent_id).first
       
+      # Check to see if the parent path is blank.
       if parent.path.blank?
+        # Add the node to the not added array.
         not_added << node
 
+        # Move to the next node.
         next
       end
 
+      # Update the node path to be the parent path plus the node id.
       node.update(path: "#{parent.path}.#{node.node_id}")
     end
 
+    # If the not added array is blank or if the not added count has not changed
+    # then we return. If the count has not changed then it is more than likely
+    # that there are data issues.
     if not_added.blank? || not_added.count == count
       return
     end
 
+    # Recursively call this method passing in the nodes that still have not been
+    # added.
     set_node_paths(not_added)
   end
   
